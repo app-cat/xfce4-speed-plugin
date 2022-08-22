5c6b6b0655c5611807a7224487f0aea5dc75ca14
yutent <yutent.io@gmail.com>
2022-08-22 16:28:56 +0800

    rename to xfce4-netload-plugin

--- panel-plugin/netload.c
+++ panel-plugin/netload.c
@@ -972,7 +972,7 @@ monitor_show_about (XfcePanelPlugin *plugin, t_global_monitor *global)
       "version", PACKAGE_VERSION,
       "program-name", PACKAGE_NAME,
       "comments", _("Monitor CPU load, swap usage and memory footprint"),
-      "website", "https://docs.xfce.org/panel-plugins/xfce4-netload-plugin/start",
+      "website", "https://docs.xfce.org/panel-plugins/xfce4-speed-plugin/start",
       "copyright", _("Copyright (c) 2003-2021\n"),
       "authors", auth, NULL);
 }
